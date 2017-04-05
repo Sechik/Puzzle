085eb54b9cdcaacf2fc97099ce818cf052ee0fbd
Aven <sechalexur@gmail.com>
2017-04-05 19:36:41 +0300

    Add project files.

---
+++ Puzzle/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Puzzle
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Image_Drop(object sender, DragEventArgs e)
+        {
+            ((Image) sender).Source = e.Source as ImageSource;
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // sender – объект, на котором произошло данное событие.
+            Image lbl = sender as Image;
+            // Создаем источник.
+            // Копируем содержимое метки Drop.
+            // 1 параметр: Элемент управления, который будет источником.
+            // 2 параметр: Данные, которые будут перемещаться.
+            // 3 параметр: Эффект при переносе.
+            DragDrop.DoDragDrop(lbl, lbl.Source, DragDropEffects.Copy);
+        }
+
+        private void Image_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effects = DragDropEffects.Copy;
+        }
+    }
+}
